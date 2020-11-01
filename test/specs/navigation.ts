48b0d53d2473ad2ed2b274ced211f6b5795e6da7
Santosh Marigowda <santosharakere@gmail.com>
2020-11-01 22:04:38 +0000

    added assertion

--- test/specs/navigation.ts
+++ test/specs/navigation.ts
@@ -1,8 +1,10 @@
 describe('Navigation Test Suite', function() {
     it('should load properly', function() {
         browser.url('/');
-        console.log(browser.getTitle());
+        if( browser.getTitle() !== 'Conduit' ) {
+            throw new Error('Title of the home page should be Conduit');
+        }
         $('=Sign in').click();
-        console.log(browser.getTitle());
+        console.log(browser.getUrl());
     });
 })
