66cd2b16518e0563fdbe47c6d4b3e9ab8372b8f1
Santosh Marigowda <santosharakere@gmail.com>
2020-11-01 21:00:01 +0000

    chromedriver and selenium-standalone services

--- wdio.conf.js
+++ wdio.conf.js
@@ -106,7 +106,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    // services: ['chromedriver'],
+    services: ['selenium-standalone'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -128,7 +129,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    reporters: ['dot', 'spec'],
 
 
     
