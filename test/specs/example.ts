f7cca458686999c91cb48fc280f375a9a5ba376c
Santosh Marigowda <santosharakere@gmail.com>
2020-11-01 21:12:05 +0000

    placeholders for 2 more tests

--- test/specs/example.ts
+++ test/specs/example.ts
@@ -1,6 +1,13 @@
-describe('webdriver.io page', () => {
+describe('page', () => {
     it('should have the right title', () => {
         browser.url('/');
         expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js')
+    });
+    it('should be able to login using valid credentials', () => {
+        // valid login code here...
+
+    });
+    it('shoud not be able to login using invalid credentials', () => {
+        // invalid login code here...
     })
 })
