5665a1b7deaff3737bd04191d1010d28b61b80e4
Santosh Marigowda <santosharakere@gmail.com>
2020-11-01 21:57:24 +0000

    fixing sync issue

--- test/specs/navigation.ts
+++ test/specs/navigation.ts
@@ -1,6 +1,8 @@
-describe('Navigation Test Suite', () => {
-    it('should load properly', () => {
+describe('Navigation Test Suite', function() {
+    it('should load properly', function() {
         browser.url('/');
+        console.log(browser.getTitle());
+        $('=Sign in').click();
         console.log(browser.getTitle());
     });
 })
