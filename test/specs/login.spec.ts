314fdf8a6d620f110ae407ce74f2993ab2890d46
Santosh Marigowda <santosharakere@gmail.com>
2020-11-02 11:45:33 +0000

    error cases

--- test/specs/login.spec.ts
+++ test/specs/login.spec.ts
@@ -1,7 +1,9 @@
 describe('Login', () => {
-    it('should let you login', () => {
+    beforeEach(() => {
         // go to the login page
         browser.url('/login');
+    })
+    it('should let you login', () => {
         // enter valid user name in email input
         $('input[type="email"]').setValue('demo@learnwebdriverio.com');
         // enter valid password in the password input
@@ -11,15 +13,28 @@ describe('Login', () => {
         // assert that we are logged in
         $('=Settings').waitForExist();
         $('=Settings').waitForDisplayed();
-        $('=Settings').scrollIntoView();
+    
         expect($('.error-messages li')).not.toBeExisting();
         expect(browser).not.toHaveUrlContaining('login');
-        console.log('break here...');
+        // logout
+        $('=Settings').scrollIntoView();
+        $('=Settings').click();
+        $('button*=logout').waitForExist();
+        $('button*=logout').scrollIntoView();
+        $('button*=logout').click();
+        expect(browser).toHaveUrl('https://demo.learnwebdriverio.com/');
+
     });
-    // it('should error with a missing username', () => {
-    //     console.log('test');
-    // });
-    // it('should error with a missing password', () => {
-    //     console.log('test');
-    // });
+    it('should error with a missing username', () => {
+        $('input[type="password"]').setValue('wdiodemo');
+        $('button*=Sign in').click();
+        expect($('.error-messages li')).toBeExisting();
+        expect($("li*=email can't be blank")).toBeExisting();
+    });
+    it('should error with a missing password', () => {
+        $('input[type="email"]').setValue('demo@learnwebdriverio.com');
+        $('button*=Sign in').click();
+        expect($('.error-messages li')).toBeExisting();
+        expect($("li*=password can't be blank")).toBeExisting();
+    });
 })
