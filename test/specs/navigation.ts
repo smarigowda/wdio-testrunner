f586dbf35c6a330cac5b2969819985a0aad20715
Santosh Marigowda <santosharakere@gmail.com>
2020-11-02 09:16:31 +0000

    assert login url

--- test/specs/navigation.ts
+++ test/specs/navigation.ts
@@ -5,6 +5,10 @@ describe('Navigation Test Suite', function() {
             throw new Error('Title of the home page should be Conduit');
         }
         $('=Sign in').click();
-        console.log(browser.getUrl());
+        const expectedLoginUrl = 'https://demo.learnwebdriverio.com/login';
+        const actualLoginUrl = browser.getUrl();
+        if(actualLoginUrl !== expectedLoginUrl) {
+            throw new Error(`URL of login page is not correct, expected url to be ${expectedLoginUrl}, actual url = ${actualLoginUrl}`);
+        }
     });
 })
