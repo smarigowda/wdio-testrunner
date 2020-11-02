59f0cb2df406dc2840eb937ca2f69f58e4a3cd52
Santosh Marigowda <santosharakere@gmail.com>
2020-11-02 09:29:24 +0000

    using built in assert library

--- test/specs/navigation.ts
+++ test/specs/navigation.ts
@@ -1,14 +1,18 @@
-describe('Navigation Test Suite', function() {
-    it('should load properly', function() {
+import assert from 'assert';
+
+describe('Navigation', function() {
+    it('Website should load properly', function() {
         browser.url('/');
-        if( browser.getTitle() !== 'Conduit' ) {
-            throw new Error('Title of the home page should be Conduit');
-        }
+        assert.strictEqual(browser.getTitle(), 'Conduit');
+        // if( browser.getTitle() !== 'Conduit' ) {
+        //     throw new Error('Title of the home page should be Conduit');
+        // }
         $('=Sign in').click();
-        const expectedLoginUrl = 'https://demo.learnwebdriverio.com/login';
-        const actualLoginUrl = browser.getUrl();
-        if(actualLoginUrl !== expectedLoginUrl) {
-            throw new Error(`URL of login page is not correct, expected url to be ${expectedLoginUrl}, actual url = ${actualLoginUrl}`);
-        }
+        // const expectedLoginUrl = 'https://demo.learnwebdriverio.com/login';
+        // const actualLoginUrl = browser.getUrl();
+        // if(actualLoginUrl !== expectedLoginUrl) {
+        //     throw new Error(`URL of login page is not correct, expected url to be ${expectedLoginUrl}, actual url = ${actualLoginUrl}`);
+        // }
+        assert.strictEqual(browser.getUrl(), 'https://demo.learnwebdriverio.com/login');
     });
 })
