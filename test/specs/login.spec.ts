8b0eaddc983ed2e4e77649ca385e36790a7b50f4
Santosh Marigowda <santosharakere@gmail.com>
2020-11-02 11:52:29 +0000

    reusable login function

--- test/specs/login.spec.ts
+++ test/specs/login.spec.ts
@@ -1,15 +1,21 @@
+function login({ username, password }) {
+    // enter valid user name in email input
+    $('input[type="email"]').setValue(username);
+    // enter valid password in the password input
+    $('input[type="password"]').setValue(password);
+    // click on sign-in button
+    $('button*=Sign in').click();
+}
+
+
 describe('Login', () => {
     beforeEach(() => {
         // go to the login page
         browser.url('/login');
     })
     it('should let you login', () => {
-        // enter valid user name in email input
-        $('input[type="email"]').setValue('demo@learnwebdriverio.com');
-        // enter valid password in the password input
-        $('input[type="password"]').setValue('wdiodemo');
-        // click on sign-in button
-        $('button*=Sign in').click();
+        login({ username: 'demo@learnwebdriverio.com', password: 'wdiodemo' });
+
         // assert that we are logged in
         $('=Settings').waitForExist();
         $('=Settings').waitForDisplayed();
@@ -23,17 +29,14 @@ describe('Login', () => {
         $('button*=logout').scrollIntoView();
         $('button*=logout').click();
         expect(browser).toHaveUrl('https://demo.learnwebdriverio.com/');
-
     });
     it('should error with a missing username', () => {
-        $('input[type="password"]').setValue('wdiodemo');
-        $('button*=Sign in').click();
+        login({ username: '', password: 'wdiodemo' });
         expect($('.error-messages li')).toBeExisting();
         expect($("li*=email can't be blank")).toBeExisting();
     });
     it('should error with a missing password', () => {
-        $('input[type="email"]').setValue('demo@learnwebdriverio.com');
-        $('button*=Sign in').click();
+        login({ username: 'demo@learnwebdriverio.com', password: '' });
         expect($('.error-messages li')).toBeExisting();
         expect($("li*=password can't be blank")).toBeExisting();
     });
