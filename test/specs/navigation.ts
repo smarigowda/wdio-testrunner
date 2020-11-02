963e48b4e9f2aabd64e8b74a33a15bf3947afbfe
Santosh Marigowda <santosharakere@gmail.com>
2020-11-02 09:38:33 +0000

    using wdio built in expect

--- test/specs/navigation.ts
+++ test/specs/navigation.ts
@@ -3,7 +3,8 @@ import assert from 'assert';
 describe('Navigation', function() {
     it('Website should load properly', function() {
         browser.url('/');
-        assert.strictEqual(browser.getTitle(), 'Conduit');
+        // assert.strictEqual(browser.getTitle(), 'Conduit');
+        expect(browser).toHaveTitle('Conduit');
         // if( browser.getTitle() !== 'Conduit' ) {
         //     throw new Error('Title of the home page should be Conduit');
         // }
@@ -13,6 +14,7 @@ describe('Navigation', function() {
         // if(actualLoginUrl !== expectedLoginUrl) {
         //     throw new Error(`URL of login page is not correct, expected url to be ${expectedLoginUrl}, actual url = ${actualLoginUrl}`);
         // }
-        assert.strictEqual(browser.getUrl(), 'https://demo.learnwebdriverio.com/login');
+        // assert.strictEqual(browser.getUrl(), 'https://demo.learnwebdriverio.com/login');
+        expect(browser).toHaveUrl('https://demo.learnwebdriverio.com/login');
     });
 })
